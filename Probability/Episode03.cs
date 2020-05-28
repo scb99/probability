f4a21ffc694ab8457162cf86921c357178dba881
Steve Bruell <bruell@cs.uiowa.edu>
2020-05-28 10:13:13 -0500

    Cleanup of Episode 3.

--- Probability/Episode03.cs
+++ Probability/Episode03.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.Linq;
+
 namespace Probability
 {
     using SCU = StandardContinuousUniform;
+
     static class Episode03
     {
         public static void DoIt()
         {
-            Console.WriteLine("Episode 03\nThe sum of 12 random doubles:");
+            Console.WriteLine("\nEpisode 03\n\nThe sum of 12 random doubles:");
             Console.WriteLine(SCU.Distribution.Samples().Take(12).Sum());
+            Console.WriteLine();
+
             Console.WriteLine("A histogram of the SCU:");
             Console.WriteLine(SCU.Distribution.Histogram(0, 1));
+            Console.WriteLine();
+
             Console.WriteLine("A histogram of a Gaussian:");
-            Console.WriteLine(Normal.Distribution(1.0, 1.5).Histogram(-4, 4));
+            Console.WriteLine(Normal.Distribution(1.0, 1.5).Histogram(-4, 6));
         }
     }
 }

833edf95bf2b689f1bd63197ddf1210ac6f171c3
Steve Bruell <bruell@cs.uiowa.edu>
2020-05-29 10:19:34 -0500

    Clean up IDiscreteDistribution.cs

--- Probability/IDiscreteDistribution.cs
+++ Probability/IDiscreteDistribution.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+
 namespace Probability
 {
     public interface IDiscreteDistribution<T> : IWeightedDistribution<T>
     {
         IEnumerable<T> Support();
+
         new int Weight(T t);
     }
 }

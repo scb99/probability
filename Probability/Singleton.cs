a2a379a007b8ff0a326100fd041385826d6e21f6
Steve Bruell <bruell@cs.uiowa.edu>
2020-05-29 10:27:34 -0500

    Clean up Singleton.cs

--- Probability/Singleton.cs
+++ Probability/Singleton.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+
 namespace Probability
 {
-    public sealed class Singleton<T> : IDiscreteDistribution<T>, IWeightedDistribution<T>
+    public sealed class Singleton<T> : 
+        IDiscreteDistribution<T>, IWeightedDistribution<T>
     {
         private readonly T t;
+
         public static Singleton<T> Distribution(T t) => new Singleton<T>(t);
+
         private Singleton(T t) => this.t = t;
+
         public T Sample() => t;
+
         public IEnumerable<T> Support() => Enumerable.Repeat(this.t, 1);
+
         public int Weight(T t) => EqualityComparer<T>.Default.Equals(this.t, t) ? 1 : 0;
+
         public override string ToString() => $"Singleton[{t}]";
+
         double IWeightedDistribution<T>.Weight(T t) => this.Weight(t);
     }
 }

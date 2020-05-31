e20474cf003e2b8d09dbe6264073281be96d48bc
Steve Bruell <bruell@cs.uiowa.edu>
2020-05-31 13:01:06 -0500

    Clean up IWeightedDistribution.cs

--- Probability/IDistribution.cs
+++ Probability/IDistribution.cs
@@ -2,6 +2,11 @@
 {
     public interface IDistribution<T>
     {
+        /// <summary>
+        /// A distibution of type T must be able to return a random value
+        /// of type T from the support of the distribution
+        /// </summary>
+        /// <returns>A random value of type T from the support of the distribution</returns>
         T Sample();
     }
 }
--- Probability/IWeightedDistribution.cs
+++ Probability/IWeightedDistribution.cs
@@ -2,6 +2,18 @@
 {
     public interface IWeightedDistribution<T> : IDistribution<T>
     {
+        /// <summary>
+        /// A weighted distribution (of type T) is a (continuous/discrete) distribution 
+        /// (of type T) that must be able to return the weight of a random value 
+        /// from the support of the distribution.
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns>If the distribution is continuous, the weight will be 1.0 if
+        /// t belongs to the support of the (continuous) distribution; 
+        /// otherwise the weight function should return 0. 
+        /// If the distribution is discrete, the weight will be the frequency of
+        /// t if t belongs to the support of the (discrete) distribution;
+        /// otherwise the weight function should return 0.</returns>
         double Weight(T t);
     }
 }

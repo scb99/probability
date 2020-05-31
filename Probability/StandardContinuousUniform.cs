d1f2fc8113f25ec36d1d4f35b53b8959ee3225d8
Steve Bruell <bruell@cs.uiowa.edu>
2020-05-31 12:32:20 -0500

    Clean up StandardContinuousUniform.cs

--- Probability/StandardContinuousUniform.cs
+++ Probability/StandardContinuousUniform.cs
@@ -3,12 +3,26 @@
     public sealed class StandardContinuousUniform :
         IWeightedDistribution<double>
     {
-        private StandardContinuousUniform() { }
+        private StandardContinuousUniform()
+        {
+        }
 
+        /// <summary>
+        /// Represents a uniform distribution of doubles over the interval [0.0, 1.0).
+        /// </summary>
         public static StandardContinuousUniform Distribution { get; set; } = new StandardContinuousUniform();
 
+        /// <summary>
+        /// Generates a sample from the standard continuous uniform distribution
+        /// </summary>
+        /// <returns>A double over interval [0.0, 1.0)</returns>
         public double Sample() => Pseudorandom.NextDouble();
 
+        /// <summary>
+        /// Determines weight of parameter
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns>1.0 if parameter is in interval [0.0, 1.0), else 0.0</returns>
         public double Weight(double t) => 0.0 <= t & t < 1.0 ? 1.0 : 0.0;
     }
 }

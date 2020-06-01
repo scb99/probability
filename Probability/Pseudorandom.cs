5d197a4bbecf9ec3b30a71049520f423e1650805
Steve Bruell <bruell@cs.uiowa.edu>
2020-05-31 22:09:16 -0500

    Clean up Pseudorandom.cs

--- Probability/Pseudorandom.cs
+++ Probability/Pseudorandom.cs
@@ -3,15 +3,28 @@ using System.Threading;
 
 namespace Probability
 {
-    // A threadsafe, all-static, crypto-randomized wrapper around Random.
-    // Still not great, but a slight improvement.
+    /// <summary>
+    /// A threadsafe, all-static, crypto-randomized wrapper around Random.
+    /// Still not great, but a slight improvement.
+    /// </summary>
     public static class Pseudorandom
     {
         private readonly static ThreadLocal<Random> prng =
             new ThreadLocal<Random>(() => new Random(BetterRandom.NextInt()));
 
+        /// <summary>
+        /// Returns a non-negative random integer.
+        /// </summary>
+        /// <returns>A 32-bit signed integer that is greater than or equal to 0
+        /// A 32-bit signed integer that is greater than or equal to 0</returns>
         public static int NextInt() => prng.Value.Next();
 
+        /// <summary>
+        /// Returns a random floating-point number that is greater than or equal to 0.0
+        /// and less than 1.0.
+        /// </summary>
+        /// <returns>A double-precision floating point number that is greater than
+        /// or equal to 0.0 and less than 1.0.</returns>
         public static double NextDouble() => prng.Value.NextDouble();
     }
 }

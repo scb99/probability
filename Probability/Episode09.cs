fc9fe9c37c2a4f0a0f177e36af4978e1635fbd0d
Steve Bruell <bruell@cs.uiowa.edu>
2020-06-01 10:50:51 -0500

    Clean up Episode09.cs

--- Probability/Episode09.cs
+++ Probability/Episode09.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using static System.Console;
+
 namespace Probability
 {
     static class Episode09
     {
         public static void DoIt()
         {
-            Console.WriteLine(WeightedInteger.Distribution(10, 0, 0, 11, 5).Histogram());
+            WriteLine(WeightedInteger.Distribution(10, 0, 0, 11, 5).Histogram());
         }
     }
 }
